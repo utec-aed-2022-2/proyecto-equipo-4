d518d741fc4bda93d24a04d7fb37fa802e503ad2
Virginia <virginia.puente@utec.edu.pe>
2022-10-05 17:27:30 -0500

    Se agrego funcion insertRegister

--- BlockChain.h
+++ BlockChain.h
@@ -60,6 +60,29 @@ void BlockChain<T>::updateRegister(int idBlock, int posRegister, T newRegister)
     blockchain->array[idBlock].front()->value->registers[posRegister] = newRegister;
 }
 
+template <typename T>
+T BlockChain<T>::insertRegister(T data)
+{
+    auto last_block = blockchain.get(last_id);
+    if (last_block.get_size() == block_size)
+    {
+        T *n_register = new T(block_size);
+        n_register[0] = data;
+        Block<T> n_block = Block<T>(last_block.get_id(), block_size, n_register, last_block.get_hash());
+        blockchain.insert(last_block.get_id() + 1, n_register);
+    }
+    else
+    {
+        blockchain.get(last_id)[blockchain.get(last_id).get_size()] = data;
+    }
+}
+
+template <typename T>
+int BlockChain<T>::get_last_id()
+{
+    return last_id;
+}
+
 
 
 #endif //PROYECTO_EQUIPO_4_BLOCKCHAIN_H
