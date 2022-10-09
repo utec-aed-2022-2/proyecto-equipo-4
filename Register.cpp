a4915a9702c9a09a6ef4f5fc162b418347dc9154
juaquin456 <juaquin.remon@utec.edu.pe>
2022-10-09 11:49:12 -0500

    Implementación de método loadFromCSV

---
+++ Register.cpp
@@ -0,0 +1,5 @@
+//
+// Created by juaquin on 09/10/22.
+//
+
+#include "Register.h"
---
+++ Register.h
@@ -0,0 +1,14 @@
+//
+// Created by juaquin on 09/10/22.
+//
+
+#ifndef PROYECTO_EQUIPO_4_REGISTER_H
+#define PROYECTO_EQUIPO_4_REGISTER_H
+
+
+class Register {
+
+};
+
+
+#endif //PROYECTO_EQUIPO_4_REGISTER_H
