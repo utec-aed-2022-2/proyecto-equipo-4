6f878542cc883c7b124229950251d647e43f081d
Johan Callinapa <85184574+johancalli@users.noreply.github.com>
2022-11-01 11:48:20 -0500

    q_Blockchain scenes

---
+++ q_Blockchain/mainwindow.cpp
@@ -0,0 +1,31 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    secDialog = new class secDialog(this);
+    secDialog->show();
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+
+    TDialog = new class TDialog(this);
+    TDialog->show();
+}
+
