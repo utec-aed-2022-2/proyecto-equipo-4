fd30ba4235d8e1266eb44c036e7c32aed868e7be
juaquin456 <juaquin.remon@utec.edu.pe>
2022-11-27 17:29:24 -0500

    formulario insertar registro

--- Dynamic/window.cpp
+++ Dynamic/window.cpp
@@ -1,6 +1,7 @@
 #include "window.h"
 #include "ui_mainwindow.h"
 #include <iostream>
+#include <chrono>
 
 Window::Window(QWidget *parent)
     : QMainWindow{parent}, ui(new Ui::MainWindow), d(new Dialog(bc))
@@ -11,11 +12,23 @@ Window::Window(QWidget *parent)
 
 void Window::on_pushButton_clicked()
 {
-    bc->print();
+    std::string emisor = ui->lineEdit->text().toStdString();
+    std::string receptor = ui->lineEdit_2->text().toStdString();
+    if (emisor!="" and receptor!=""){
+        std::string monto = ui->lineEdit_3->text().toStdString();
+        auto p = std::chrono::system_clock::now();
+        std::string fecha = std::to_string(std::chrono::duration_cast<std::chrono::seconds>(p.time_since_epoch()).count());
+        this->bc->insertRegister(Register(emisor+","+receptor+","+monto+","+fecha));
+        this->bc->print();
+    }
 }
 
 
 void Window::on_pushButton_clicked(bool checked)
 {
     std::cout << checked << std::endl;
+}
+
+Window::~Window(){
+    delete bc;
 }
--- Dynamic/window.h
+++ Dynamic/window.h
@@ -15,6 +15,7 @@ class Window : public QMainWindow
     Q_OBJECT
 public:
     explicit Window(QWidget *parent = nullptr);
+    ~Window();
 
 signals:
 private slots:
